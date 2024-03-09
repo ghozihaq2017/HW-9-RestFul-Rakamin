26c2cfc98244533bbb51ee8dbfaecd194ab82c68
Ghozi Izzulhaq <ghozihaq2017@gmail.com>
2024-03-09 23:20:20 +0700

    feat: add swagger

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 3000;
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+const morgan = require('morgan');
+
+app.use(morgan('tiny'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
